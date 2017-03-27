e39fa60d6b0ca636e79244b29deb1dcab80aefce
AlbertoEyo <alberto.eyo.contractor@bbva.com>
2017-03-27 12:48:56 +0200

    Format
    Format del fichero

--- test/step-sayHello.js
+++ test/step-sayHello.js
@@ -4,13 +4,15 @@ const expect = require('chai').expect;
 const exec = require('child_process').exec;
 
 describe('::sayHello validation', function() {
-  this.timeout(5000);
-  it('Should \'::sayHello\' works', (done) => {
-    exec('node ../.. ::sayHello', { cwd: 'test/world' }, (error, stdout, stderr) => {
-      expect(error).to.equal(null);
-      expect(stderr).to.equal('');
-      expect(stdout).contain('HELLO WORLD');
-      done();
+    this.timeout(5000);
+    it('Should \'::sayHello\' works', (done) => {
+        exec('node ../.. ::sayHello', {
+            cwd: 'test/world'
+        }, (error, stdout, stderr) => {
+            expect(error).to.equal(null);
+            expect(stderr).to.equal('');
+            expect(stdout).contain('HELLO WORLD');
+            done();
+        });
     });
-  });
 });
